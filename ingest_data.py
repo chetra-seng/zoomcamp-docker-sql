303a7ea388dbc7041b62b9c543aa600a57bdd35d
Chetra Seng <dev@chetraseng.com>
2026-01-20 23:12:51 +0700

    updated ingest_data to support commandline

--- ingest_data.py
+++ ingest_data.py
@@ -5,23 +5,24 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click
 
 prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download"
 
-year = "2021"
-month = "01"
-url = f"{prefix}/yellow/yellow_tripdata_{year}-{month}.csv.gz"
 
-db_user = "root"
-db_password = "root"
-db_host = "localhost"
-db_port = 5432
-db_name = "ny_taxi"
+@click.command()
+@click.option("--user", default="root", help="PostgreSQL user")
+@click.option("--password", default="root", help="PostgreSQL password")
+@click.option("--host", default="localhost", help="PostgreSQL host")
+@click.option("--port", default=5432, type=int, help="PostgreSQL port")
+@click.option("--db", default="ny_taxi", help="PostgreSQL database name")
+@click.option("--table", default="yellow_taxi_data", help="Target table name")
+@click.option("--year", default="2021", help="Year of taxi data")
+@click.option("--month", default="01", help="Month of taxi data (01-12)")
+@click.option("--chunksize", default=100000, type=int, help="Number of rows per chunk")
+def run(user, password, host, port, db, table, year, month, chunksize):
+    url = f"{prefix}/yellow/yellow_tripdata_{year}-{month}.csv.gz"
 
-target_table = "yellow_taxi_data"
-
-
-def run():
     dtype = {
         "VendorID": "Int64",
         "passenger_count": "Int64",
@@ -43,27 +44,23 @@ def run():
 
     parse_dates = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
 
-    engine = create_engine(
-        f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
-    )
+    engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}")
 
     df_iter = pd.read_csv(
         url,
         dtype=dtype,  # type: ignore
         parse_dates=parse_dates,
         iterator=True,
-        chunksize=100000,
+        chunksize=chunksize,
     )
 
     first = True
     for df_chunk in tqdm(df_iter):
         if first:
-            df_chunk.head(n=0).to_sql(
-                name=target_table, con=engine, if_exists="replace"
-            )
+            df_chunk.head(n=0).to_sql(name=table, con=engine, if_exists="replace")
             first = False
 
-        df_chunk.to_sql(name=target_table, con=engine, if_exists="append")
+        df_chunk.to_sql(name=table, con=engine, if_exists="append")
 
 
 if __name__ == "__main__":
